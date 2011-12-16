fc8bcb030538a8f5b734473c55ff0c50527afb1b
Derick Bailey <derickbailey@gmail.com>
2011-12-15 23:23:02 -0600

    code comments for backbone.mailapp.js

--- public/javascripts/bbclonemail.mailapp.js
+++ public/javascripts/bbclonemail.mailapp.js
@@ -1,23 +1,43 @@
+// This is the "controller" or sub-application
+// for the mail app. It contains all of the 
+// high level knowledge of how to run the app
+// when it's in mail mode.
 BBCloneMail.MailApp = (function(BBCloneMail, Backbone){
   var MailApp = {};
 
+  // Email model and collection. These are pretty much
+  // self explanatory.
   MailApp.Email = Backbone.Model.extend({});
 
   MailApp.EmailCollection = Backbone.Collection.extend({
     model: MailApp.Email
   });
 
+  // This is a "MailApp" method that is called whenever
+  // we are switching the BBMailCone app into mail mode.
+  // It replaces all of the visual regions with the 
+  // correct content by calling the `show` method for the
+  // correct region manager.
   MailApp.show = function(){
+
+    // Show the mail box with email entries
     BBCloneMail.mainRegion.show(new BBCloneMail.MailApp.MailBox({
       collection: MailApp.emailList
     }));
 
+    // Show the mail categories list
     BBCloneMail.navigationRegion.show(new BBCloneMail.MailApp.Categories());
 
+    // Updates the url's #hash fragment with the correct hash
     BBCloneMail.showRoute("inbox");
+
+    // Updates the app mode select box
     BBCloneMail.AppSelection.showSelection("mail");
   };
 
+  // Initializes the email collection object with the list
+  // of emails that are passed in from the call to 
+  // `BBCloneMail.start`.
   BBCloneMail.addInitializer(function(options){
     MailApp.emailList = new MailApp.EmailCollection(options.email);
   });
