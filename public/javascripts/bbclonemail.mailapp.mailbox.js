857a2292a54bcfc35e363f1383e6d9880f4af2d5
Derick Bailey <derickbailey@gmail.com>
2011-12-15 23:26:01 -0600

    code comments for backbone.mailapp.mailbox.js

--- public/javascripts/bbclonemail.mailapp.mailbox.js
+++ public/javascripts/bbclonemail.mailapp.mailbox.js
@@ -1,4 +1,11 @@
+// The mail box view to display the list of emails
+// for the mailbox.
 BBCloneMail.MailApp.MailBox = (function(Backbone, $){
+
+  // The item view is private to the MailBox module
+  // because the rest of the app does not need to
+  // know about it. Only the mailbox itself needs to
+  // be concerned with the individual item view.
   var ItemView = Backbone.View.extend({
     tagName: "li",
     template: "#email-list-item-template",
@@ -7,24 +14,27 @@ BBCloneMail.MailApp.MailBox = (function(Backbone, $){
       "click": "showHideBody"
     },
 
+    // Show or hide the body of the email when
+    // the email header is clicked.
     showHideBody: function(e){
       $(this.el).find(".body").toggle("fast");
     }
   });
 
+  // The actual mail box view, which renders each
+  // of the individual email items. You can see here
+  // that I'm overriding the `render` method in order
+  // to render the collection of item views.
   return Backbone.View.extend({
     tagName: "ul",
     className: "email-list",
 
     initialize: function(){
-      _.bindAll(this, "render", "renderItem");
+      _.bindAll(this, "renderItem");
     },
 
     events: {
       "click header": "expandCollapseEmail"
-    },
-
-    expandCollapseEmail: function(e){
     },
 
     render: function(){
