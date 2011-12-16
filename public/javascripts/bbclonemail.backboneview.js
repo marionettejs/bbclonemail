a0239628c9f54e646bec2dbfe8a7122532a9c46c
Derick Bailey <derickbailey@gmail.com>
2011-12-16 06:46:51 -0600

    moving backboneview mod to it's own file

---
+++ public/javascripts/bbclonemail.backboneview.js
@@ -0,0 +1,30 @@
+// Backbone.BBCloneMail
+// A reference application for Backbone.Marionette
+//
+// Copyright (C)2011 Derick Bailey, Muted Solutions, LLC
+// Distributed Under MIT License
+//
+// Documentation and Full License Available at:
+// http://github.com/derickbailey/backbone.bbclonemail
+// http://github.com/derickbailey/backbone.marionette
+
+// Backbone View Rendering
+// -----------------------
+
+// Rendering is 99% the same across all of my
+// views, so I've encapsulated the logic of it
+// here, by providing a prototype render method
+// on `Backbone.View`. Of course, this can be 
+// overridden in a specific view instance, which
+// you'll see in other places.
+Backbone.View.prototype.render = function(){
+  var data;
+
+  if (this.model){data = this.model.toJSON();}
+  if (this.collection){data = this.collection.toJSON();}
+
+  var html = $(this.template).tmpl(data);
+  $(this.el).html(html);
+}
+
+
