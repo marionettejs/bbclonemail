60359999fc961042f24d826f20bd1d072548a39c
Derick Bailey <derickbailey@gmail.com>
2012-02-07 11:33:09 -0600

    code comments

--- public/javascripts/bbclonemail.views.js
+++ public/javascripts/bbclonemail.views.js
@@ -28,15 +28,23 @@ Backbone.Marionette.ItemView.prototype.renderTemplate = function(template, data)
 (function(){
   var promises = {};
 
-  var getPromise = function(tmpId){
+  // Cache the returned deferred/promise by the id of the template
+  // so that we can prevent multiple requests for the same template
+  // from making the ajax call.
+  //
+  // This code is only safe to run synchronously. There exists a
+  // race condition in this function, when run asynchronously,
+  // which would nullify the benefit under certain circumstances.
+  var loadTemplateAsync = function(tmpId){
     var promise = promises[tmpId] || $.get("/templates/" + tmpId + ".html");
     promises[tmpId] = promise;
     return promise;
   }
 
+  // Use jQuery to asynchronously load the template. 
   Backbone.Marionette.TemplateManager.loadTemplate = function(templateId, callback){
     var tmpId = templateId.replace("#", "");
-    var promise = getPromise(tmpId);
+    var promise = loadTemplateAsync(tmpId);
     promise.done(function(template){
       callback.call(this, $(template));
     });
