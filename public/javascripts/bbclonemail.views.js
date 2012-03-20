cdb2705051024af3c283941493d29041e0b1c758
Derick Bailey <derickbailey@gmail.com>
2012-03-19 23:53:38 -0500

    replacing a bunch of code w/ the TrafficCop plugin, to ensure we only load a template from the server once

--- public/javascripts/bbclonemail.views.js
+++ public/javascripts/bbclonemail.views.js
@@ -24,29 +24,14 @@ Backbone.Marionette.ItemView.prototype.renderTemplate = function(template, data)
 // Set up async template loading from the server. A view with
 // a template of `"#my-view-template"` will load a file called
 // `"/templates/my-view-template.html"` from the server.
-
-(function(){
-  var promises = {};
-
-  // Cache the returned deferred/promise by the id of the template
-  // so that we can prevent multiple requests for the same template
-  // from making the ajax call.
-  //
-  // This code is only safe to run synchronously. There exists a
-  // race condition in this function, when run asynchronously,
-  // which would nullify the benefit under certain circumstances.
-  var loadTemplateAsync = function(tmpId){
-    var promise = promises[tmpId] || $.get("/templates/" + tmpId + ".html");
-    promises[tmpId] = promise;
-    return promise;
-  }
-
-  // Use jQuery to asynchronously load the template. 
-  Backbone.Marionette.TemplateCache.loadTemplate = function(templateId, callback){
-    var tmpId = templateId.replace("#", "");
-    var promise = loadTemplateAsync(tmpId);
-    promise.done(function(template){
-      callback.call(this, $(template));
-    });
-  }
-})();
+//
+// Use the TrafficCop plugin to ensure we only make one
+// request to get the template
+Backbone.Marionette.TemplateCache.loadTemplate = function(templateId, callback){
+  var tmpId = templateId.replace("#", "");
+  var url = "/templates/" + tmpId + ".html";
+  var promise = $.trafficCop(url);
+  promise.done(function(template){
+    callback.call(this, $(template));
+  });
+}
