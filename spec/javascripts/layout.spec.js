817c4060c1a111d16d5c1f98923b0926fa52b7f1
Derick Bailey <derickbailey@gmail.com>
2012-07-07 14:22:21 -0500

    first idea of first test, with not implemented error

---
+++ spec/javascripts/layout.spec.js
@@ -0,0 +1,10 @@
+describe("application layout", function(){
+
+  describe("when the application starts", function(){
+
+    it("should attach the layout to the main content", function(){
+      throw "not implemented"
+    });
+  });
+
+});
