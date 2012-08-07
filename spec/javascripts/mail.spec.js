5b680bbbc76a6b761fe3458f97fd6208d9901ab0
Derick Bailey <derickbailey@gmail.com>
2012-08-07 08:42:06 -0500

    adding shell of first specs for mail module

---
+++ spec/javascripts/mail.spec.js
@@ -0,0 +1,13 @@
+describe("mail", function(){
+
+  describe("when getting mail for the inbox", function(){
+    it("should call the server to get the mail", function(){
+      throw "not implemented";
+    });
+
+    it("should return a collection with all the mail in it", function(){
+      throw "not implemented";
+    });
+  });
+
+});
