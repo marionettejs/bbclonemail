d28f832e36918b15ed1f33e3109c87e2047585ad
Derick Bailey <derickbailey@gmail.com>
2012-08-26 14:57:48 -0500

    removing debug stuff

--- app.rb
+++ app.rb
@@ -4,14 +4,6 @@ require 'sinatra/reloader' if development?
 require 'haml'
 require 'json'
 
-# set :environment, :production
-
-if development?
-  puts "DEV MODE!!!"
-else
-  puts "another mode"
-end
-
 get "/" do
   haml :layout
 end
