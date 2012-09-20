3aa00c6d9e9968da9f437a523322e45b8f3ecb24
Derick Bailey <derickbailey@gmail.com>
2012-09-19 20:52:11 -0500

    moving req/res and command impls in to marionette.wreqr.js

---
+++ public/javascripts/vendor/marionette.wreqr.js
@@ -0,0 +1,82 @@
+// Marionette.Wreqr v0.0.0
+// -----------------------
+// A basic Request/Response and Command pattern implementation
+// for Backbone.js applications
+//
+// Copyright 2012 Muted Solutions, LLC. All Rights Reserved
+
+// Request/Response
+// ----------------
+
+(function(Marionette){
+  "use strict";
+
+  var handlers = {};
+
+  _.extend(Marionette.Application.prototype, {
+    respondTo: function(name, handler, context){
+      var config = {
+        handler: handler,
+        context: context
+      };
+
+      handlers[name] = config;
+    },
+
+    request: function(name, args){
+      var config = handlers[name];
+
+      if (!config){
+        throw new Error("Request handler not found for '" + name + "'");
+      }
+
+      return config.handler.call(config.context, args);
+    },
+
+    removeRequestHandler: function(name){
+      delete handlers[name];
+    },
+
+    clearRequestHandlers: function(){
+      handlers = {};
+    }
+  });
+
+})(Backbone.Marionette);
+
+// Command Pattern
+// ---------------
+
+(function(Marionette){
+  "use strict";
+
+  var handlers = {};
+
+  _.extend(Marionette.Application.prototype, {
+
+    registerCommand: function(name, handler, context){
+      handlers[name] = {
+        handler: handler,
+        context: context
+      };
+    },
+
+    removeCommand: function(name){
+      delete handlers[name];
+    },
+
+    clearCommands: function(){
+      handlers = {};
+    },
+
+    execute: function(name, args){
+      var config = handlers[name];
+      if (!config){
+        throw new Error("Handler not found for '" + name + "'");
+      }
+
+      config.handler.call(config.context, args);
+    }
+  });
+
+})(Backbone.Marionette);
